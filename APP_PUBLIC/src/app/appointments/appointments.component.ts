3a315bbbd7804fdeea343f90baf129f518711f9c
Fernanda Meneses Oliveira <f.meneses.oliveira@gmail.com>
2022-08-06 10:02:40 -0400

    My appointments page

--- APP_PUBLIC/src/app/appointments/appointments.component.ts
+++ APP_PUBLIC/src/app/appointments/appointments.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { Appointment } from '../models/appointment';
+import { AppointmentService } from '../services/appointmentservice';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-appointments',
   templateUrl: './appointments.component.html',
   styleUrls: ['./appointments.component.css']
 })
-export class AppointmentsComponent implements OnInit {
+export class AppointmentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private AppointmentService: AppointmentService, private router: Router, private toastr: ToastrService){
 
-  ngOnInit(): void {
   }
 
+  //I need to know the role of the user logged in
+  //if the role is Professional I have to search by doctorName
+  //if the role is Client I have to search by clientName
+
+  ngOnInit(): void {
+
+  }
+
+  /*
+    public fetchAppointments (newAppointment: Appointment): void{
+      this.AppointmentService.getAppointments(newAppointment).then((response) => {
+        if(response) {
+
+        }
+      });
+    }
+*/
+  public buttonMessage() {
+    this.toastr.info('Implementent in next version!');
+  }
 }
